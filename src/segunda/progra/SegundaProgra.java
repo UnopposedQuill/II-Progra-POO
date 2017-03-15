3cf3c074e7a5fef1633d0c84bba37bb55df1c9b3
UnopposedQuill <UnopposedQuill@github.com>
2017-03-15 12:05:51 -0600

    Modificaciones dentro de un main para recibir caracteres de la entrada estándar

--- src/segunda/progra/SegundaProgra.java
+++ src/segunda/progra/SegundaProgra.java
@@ -6,6 +6,9 @@
 package segunda.progra;
 
 import Gui.FrameAdministrador;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,5 +23,18 @@ public class SegundaProgra {
         // TODO code application logic here
         System.out.println("Iniciando Programa");
         FrameAdministrador ventanaAdministradora = new FrameAdministrador();
+        /*
+        byte[] arreglo = {};
+        try {
+            System.in.read();
+            System.in.read(arreglo);
+        } catch (IOException ex) {
+            Logger.getLogger(SegundaProgra.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        for (int i = 0; i < arreglo.length; i++) {
+            byte b = arreglo[i];
+            char cha = (char)b;
+        }
+        */
     }
 }
